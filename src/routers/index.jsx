19fab0656044cf808b9034ac541f0c2b15c0e09d
nguyenhuugiang08 <77537697+nguyenhuugiang08@users.noreply.github.com>
2022-08-10 09:45:47 +0700

    Update index.jsx

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -58,7 +58,7 @@ function Routerall() {
   })
 
   return (
-    <div style={{ width: 'calc(100% - 240px)', position: 'relative', left: '240px', padding: '70px 60px 0', top: '0', marginBottom: `${mounted ? "90px" : ""}`, backgroundColor: '#170f23', minHeight: 'calc(100vh - 90px)' }}>
+    <div style={{ width: 'calc(100% - 240px)', position: 'relative', left: '240px', padding: '70px 60px 0', top: '0', marginBottom: `${mounted ? "90px" : ""}`, backgroundColor: '#170f23', minHeight: '100vh' }}>
       <Routes>
         {routes.map((route, index) => {
           const Page = route.component
